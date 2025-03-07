3fa696826d3210c343a63b316ffaa200ad95769d
Thomas Klughardt <thomas.klughardt@googlemail.com>
2025-03-07 19:35:39 +0100

    really allow public access

--- current_working/infra_terraform/s3.tf
+++ current_working/infra_terraform/s3.tf
@@ -24,3 +24,46 @@ resource "aws_s3_bucket_public_access_block" "backup" {
   ignore_public_acls      = false
   restrict_public_buckets = false
 }
+
+# really really publically accessible
+resource "aws_s3_bucket_policy" "public_read" {
+  bucket = aws_s3_bucket.backup.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.backup.arn}/*"
+      }
+    ]
+  })
+
+  depends_on = [aws_s3_bucket_public_access_block.backup]
+}
+
+resource "aws_s3_bucket" "backup_replication" {
+  provider = aws.secondary_region
+  bucket   = "${var.project_name}-backup-bucket-replication"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_versioning" "backup" {
+  bucket = aws_s3_bucket.backup.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "backup_replication" {
+  provider = aws.secondary_region
+  bucket = aws_s3_bucket.backup_replication.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+
