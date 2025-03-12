314701b4803ca9aa63a82e8c4ca582ae5a132371
Thomas Klughardt <thomas.klughardt@googlemail.com>
2025-03-12 02:48:09 +0100

    destroy all

--- current_working/infra_terraform/eks.tf
+++ current_working/infra_terraform/eks.tf
@@ -51,12 +51,36 @@ resource "aws_iam_role_policy_attachment" "additional" {
   role       = each.value.iam_role_name
 }
 
-provider "helm" {
-  kubernetes {
-    host                   = data.aws_eks_cluster.cluster.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-    token                  = data.aws_eks_cluster_auth.cluster.token
+resource "helm_release" "ingress" {
+  name       = "ingress"
+  chart      = "aws-load-balancer-controller"
+  repository = "https://aws.github.io/eks-charts"
+  version    = "1.4.6"
+
+  namespace  = "kube-system"
+
+  set {
+    name  = "autoDiscoverAwsRegion"
+    value = "true"
   }
+  set {
+    name  = "autoDiscoverAwsVpcID"
+    value = "true"
+  }
+  set {
+    name  = "clusterName"
+    value = local.cluster_name
+  }
+  set {
+    name  = "serviceAccount.create"
+    value = "false"
+  }
+  set {
+    name  = "serviceAccount.name"
+    value = "aws-load-balancer-controller"
+  }
+
+  depends_on = [module.eks]
 }
 
 resource "helm_release" "ingress" {
