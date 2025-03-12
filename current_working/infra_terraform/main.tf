23d655ea96c9fbc2f586e3ebe43e0883c10fd316
Thomas Klughardt <thomas.klughardt@googlemail.com>
2025-03-12 09:04:20 +0100

    removed duplicate

--- current_working/infra_terraform/main.tf
+++ current_working/infra_terraform/main.tf
@@ -29,12 +29,6 @@ locals {
   cluster_name = "workwiz"
 }
 
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-
-}
 
 module "eks-kubeconfig" {
   source  = "hyperbadger/eks-kubeconfig/aws"
