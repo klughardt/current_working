b21c4809bef32159a3e9257a09642cfeda3e0fe3
Thomas Klughardt <thomas.klughardt@googlemail.com>
2025-03-14 14:01:23 +0100

    egress back in

--- current_working/infra_terraform/mongodb.tf
+++ current_working/infra_terraform/mongodb.tf
@@ -4,16 +4,16 @@ resource "aws_security_group" "mongodb" {
   vpc_id      = module.vpc.vpc_id
 
   ingress {
-    from_port   = 27017
-    to_port     = 27017
-    protocol    = "tcp"
-    cidr_blocks = [var.vpc_cidr]
-  }
-
-  ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
