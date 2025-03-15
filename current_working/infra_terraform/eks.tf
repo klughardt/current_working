a7a84ca68f79ce484b5f020f10c14621573aa0fe
Thomas Klughardt <thomas.klughardt@googlemail.com>
2025-03-15 13:17:38 +0100

    explicit sec group

--- current_working/infra_terraform/eks.tf
+++ current_working/infra_terraform/eks.tf
@@ -20,12 +20,12 @@ module "eks" {
 
   node_security_group_additional_rules = {
     ingress_allow_access_from_control_plane = {
-      type                          = "ingress"
-      protocol                      = "tcp"
-      from_port                     = 9443
-      to_port                       = 9443
-      source_cluster_security_group = true
-      description                   = "Allow access from control plane to webhook port of AWS load balancer controller"
+      type              = "ingress"
+      protocol          = "tcp"
+      from_port         = 9443
+      to_port           = 9443
+      security_group_id = module.eks.cluster_security_group_id
+      description       = "Allow access from control plane to webhook port of AWS load balancer controller"
     }
   }
 }
