137b2e1415189739751f97735d1c88625bab0f40
Thomas Klughardt <thomas.klughardt@googlemail.com>
2025-03-15 09:40:48 +0100

    going up for ha

--- current_working/infra_terraform/eks.tf
+++ current_working/infra_terraform/eks.tf
@@ -11,7 +11,7 @@ module "eks" {
 
   eks_managed_node_groups = {
     workwiz_app = {
-      desired_capacity = 1
+      desired_capacity = 3
       max_capacity     = 3
       min_capacity     = 1
       instance_type    = "t3.small"
