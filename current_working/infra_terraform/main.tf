fa60f3e253338cd54add6419c81b39c0bef127e6
Thomas Klughardt <thomas.klughardt@googlemail.com>
2025-03-17 13:43:30 +0100

    fixing further

--- current_working/infra_terraform/main.tf
+++ current_working/infra_terraform/main.tf
@@ -27,8 +27,6 @@ provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
   token                  = data.aws_eks_cluster_auth.cluster.token  # Use the one from main.tf
-
-  depends_on = [module.eks]
 }
 
 provider "helm" {
