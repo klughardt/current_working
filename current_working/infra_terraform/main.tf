b961c5e194aece38aa5db0a73e21df3413e49a2f
Thomas Klughardt <thomas.klughardt@googlemail.com>
2025-03-17 13:54:27 +0100

    typo fixed

--- current_working/infra_terraform/main.tf
+++ current_working/infra_terraform/main.tf
@@ -25,17 +25,18 @@ data "aws_eks_cluster_auth" "cluster" {
 
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster_certificate_authority_data)
-  token                  = data.aws_eks_cluster_auth.cluster.token  # Use the one from main.tf
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
 }
 
 provider "helm" {
   kubernetes {
     host                   = data.aws_eks_cluster.cluster.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster_certificate_authority_data)
-    token                  = data.aws_eks_cluster_auth.cluster.token  # Use the one from main.tf
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
   }
 }
+
 
 data "aws_availability_zones" "available" {}
 
