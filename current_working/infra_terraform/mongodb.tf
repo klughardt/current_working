4fa43366fec23e30992a44c33a3f22608bb34914
Thomas Klughardt <thomas.klughardt@googlemail.com>
2025-03-17 22:33:28 +0100

    push without commented rule

--- current_working/infra_terraform/mongodb.tf
+++ current_working/infra_terraform/mongodb.tf
@@ -10,14 +10,6 @@ resource "aws_security_group" "mongodb" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ## Allow access to MongoDB on port 27017 from whole VPC
-  #ingress {
-  #  from_port   = 27017
-  #  to_port     = 27017
-  #  protocol    = "tcp"
-  #  cidr_blocks = [var.vpc_cidr]
-  #}
-
   egress {
     from_port   = 0
     to_port     = 0
